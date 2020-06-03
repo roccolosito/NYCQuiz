582f3c63049ca9595b7b9c28f57d7ed0a952cdfb
Rocco Losito <roccolosito@gmail.com>
2020-06-03 01:17:20 -0400

    added questions var to script.js, updated README.md, fine-tuned the timer function.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A timed quiz with multiple-choice questions on the topic of NYC History. There are 4 questions, and 60 seconds to answer all 4. Every incorrect answers shaves off 5 seconds 
 from the timer. The user's final score is equal to the number of seconds left on the timer at the end of the game. 
 
+Link for the deployed app: https://roccolosito.github.io/NYCQuiz/
+
+Screenshots:
+
--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
         var interval = setInterval(function () {
             counter--;
             if (counter <= 0) {
-                clearInterval(interval);
-                return;
+                console.log("Timer 0 --> " + counter);
+                return clearInterval(interval);
             } else {
                 $('#time').text(counter);
                 console.log("Timer --> " + counter);
@@ -17,15 +17,50 @@ $(document).ready(function () {
         }, 1000);
         $("#start").hide();
         $("#jumbotron").hide();
+        $("#quiz").show();
+        showQuestion();
     });
 
-    // -> ...while appending remaining time in seconds to the resulted score of the user
+    function showQuestion() {
+        var prompt = questions[currentQuestion].prompt;
+        $("#quiz h2").text(prompt);
+        $("#quiz ul").html("");
+        for (var i = 0; i < parseInt(questions.length); i++) {
+            var show = questions[currentQuestion].prompt[i];
+            $("#quiz ul").append(`<li class="button-select" id="${i}">${show}</li>`);
+        }
 
-    function timeUp() {
-        $("#timer").text("expired!");
-        $(".quiz").hide();
-        $(".scoreContainer").show();
-        $("#scoreNumber").append(secondsLeft);
-        clearInterval(xInterval);
-    };
+        var questions = [
+            {
+                prompt: "How many languages are spoken in NYC?\n(a) 800\n(b) 127\n(c) 232",
+                answer: "a"
+            },
+            {
+                prompt: "What is the name of the group of people indigenous to NYC?\n(a) Algonquin\n(b) Apache\n(c) Lenape",
+                answer: "c"
+            },
+            {
+                prompt: "Who was known as the Father of Harlem?\n(a) WEB DuBois\n(b) Philip Payton Jr\n(c) Malcolm X",
+                answer: "b",
+            },
+            {
+                prompt: "The first African American to play on the MLB was on the Brooklyn Dodgers, what was his name?\n(a) Willie Mays\n(b) Barry Bonds\n(c) Jackie Robinson",
+                answer: "c",
+            },
+        ];
+
+        // var score = 0;
+
+        // for (var i = 0; i < questions.length; i++) {
+        //     var response = questions[i].prompt;
+        //     if (response == questions[i].answer) {
+        //         score++;
+        //         alert("Got it!");
+        //     } else {
+        //         alert("Nope!");
+        //     }
+        // }
+        // alert("you got " + score + "/" + questions.length);
+
+    }
 })
